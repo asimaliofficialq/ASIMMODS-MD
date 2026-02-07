6f9e6efa33e99432b0603e026565bbe94ff6a5ca
asimaliofficialq <awaseem9001@gmail.com>
2026-02-07 20:46:58 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 ᴬˢᴵᴹ-ᴹᴼᴰˢ 𓆪',
+  author: '‎',
+  botName: "𓆩 ᴬˢᴵᴹ-ᴹᴼᴰˢ 𓆪",
+  botOwner: 'ASIM', // Your name
+  ownerNumber: '923096460027', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/silvertechx13/---",
+};
+
+module.exports = settings;
